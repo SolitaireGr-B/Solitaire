a3010f2291d4ac78e5f32a8dfa708ecf68675eac
Brok3nBass <35891882+Brok3nBass@users.noreply.github.com>
2018-01-31 12:53:20 +0100

    Initial commit
    Stuff1

---
+++ src/main/java/nl/quintor/solitaire/ui/MyUI.java
@@ -0,0 +1,37 @@
+package nl.quintor.solitaire.ui;
+
+import nl.quintor.solitaire.game.moves.Move;
+import nl.quintor.solitaire.game.moves.Quit;
+import nl.quintor.solitaire.models.state.GameState;
+
+import java.util.Collection;
+import java.util.Scanner;
+
+public class MyUI implements UI{
+    public static void readInput(){
+        Scanner input = new Scanner(System.in);
+        if(input.next() == "q"){
+            new Quit();
+        }
+    }
+    @Override
+    public void setMessage(String message) {
+    System.out.print(message);
+    }
+
+    @Override
+    public void setErrorMessage(String message) {
+    System.out.print(message);
+    }
+
+    @Override
+    public void refresh(GameState gameState) {
+    System.out.print(gameState);
+    }
+
+    @Override
+    public String refreshAndRequestMove(GameState gameState, Collection<Move> moves) {
+        gameState.getMoves();
+        return moves.toString();
+    }
+}
